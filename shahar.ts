4e9d5bc1e7711b6b6037c56ae1d5ff9528ffdffa
Shahar Yakir <shahar.yakir@gmail.com>
2022-12-31 18:28:52 +0200

    -

--- shahar.ts
+++ shahar.ts
@@ -2,4 +2,12 @@ function hello() {
   return "hello";
 }
 
-console.log(hello());
+class Calc {
+  add(num1: number, num2: number) {
+    return num1 + num2;
+  }
+}
+
+const calc = new Calc();
+
+console.log(calc.add(2,4));
